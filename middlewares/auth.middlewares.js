1f34ef331d09dd5b426aa674ca41f7efb6a0105f
Yair Pantoja <pantoja1144yair@gmail.com>
2026-08-21 22:27:06 -0500

    Update auth.middlewares.js

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -5,7 +5,7 @@ const verificarToken = async(req, res, next) =>{
         const authHeader = req.headers["authorization"];
 
         if(!authHeader){
-            return res.status(403).json({message: "Se requiere token"});
+            return res.status(401).json({message: "Se requiere token"});
         }
 
         const token = authHeader.split(' ')[1];
