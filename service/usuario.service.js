5c31814b23a3403267536aa82a5fa2737174d340
Yair Pantoja <pantoja1144yair@gmail.com>
2026-08-21 22:10:14 -0500

    Update usuario.service.js

--- service/usuario.service.js
+++ service/usuario.service.js
@@ -16,7 +16,7 @@ try{
 
     if(!result.insertId) {
         const error = new Error("Error al crear usuario");
-        error.status = 400;
+        error.status = 500;
         throw error;
     }
 
